7c1a2849b9cf04a1babd5d6375185d7fcb4536ce
Alexander Baldwin <alexanderryanbaldwin@gmail.com>
2020-07-24 13:55:05 -0400

    heroku is hard

--- index.js
+++ index.js
@@ -46,7 +46,7 @@ client.on('ready', () => {
 	//!mws setid 1892900
 });
 
-client.login(process.env.BOT_TOKEN);
+client.login(process.env.CLIENT_TOKEN);
 
 client.on('message', async msg => {
 	try {
